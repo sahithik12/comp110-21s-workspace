f5cb90a9302450f529752aa7f4074af7918f3806
Sahithi Korrapati <sahithi@live.unc.edu>
2021-02-07 12:46:33 -0500

    fortune cookie done!

--- exercises/ex01/fortune_cookie.py
+++ exercises/ex01/fortune_cookie.py
@@ -2,14 +2,21 @@
 
 __author__ = "ENTER YOUR 9-DIGIT PID HERE"
 
-# The randint function is imported from the random library so that
-# you are able to generate integers at random.
-# 
-# Documentation: https://docs.python.org/3/library/random.html#random.randint
-#
-# For example, consider the function call expression: randint(1, 100)
-# It will evaluate to an int value >= 1 and <= 100. 
 from random import randint
 
 
-# Begin your solution here...
+number: int = ((randint(1, 4)))
+
+if number == 1:
+    print("A beautiful, smart, and loving person will be coming into your life.")
+else:
+    if number == 2:
+        print("Your life will be happy and peaceful.")
+    else:
+        if number == 3:
+            print("Soon life will become more interesting.")
+        else:
+            if number == 4:
+                print("You are already on the right path to happiness and success.")
+
+print("Now, go spread positive vibes!")
